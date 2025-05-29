6392be4efc74ab1b7ea5d720b95bc7b324629a48
Zak Brinlee <zbrinlee@gmail.com>
2025-05-28 19:10:01 -0700

    cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,18 +29,6 @@ export const ThemeContext = createContext<ThemeContextType>({
 
 const queryClient = new QueryClient();
 
-// Tells Supabase Auth to continuously refresh the session automatically if
-// the app is in the foreground. When this is added, you will continue to receive
-// `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
-// if the user's session is terminated. This should only be registered once.
-// AppState.addEventListener('change', (state) => {
-//   if (state === 'active') {
-//     supabase.auth.startAutoRefresh()
-//   } else {
-//     supabase.auth.stopAutoRefresh()
-//   }
-// })
-
 export default function RootLayout() {
   const [colorMode, setColorMode] = useState<ModeType>("light");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
